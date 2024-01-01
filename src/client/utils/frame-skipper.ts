5a86663e18b729ddbb7ab997ec03c0972ce27748
Bursztynek98 <44409970+Bursztynek98@users.noreply.github.com>
2024-01-01 05:08:24 +0100

    Add FrameSkipper

---
+++ src/client/utils/frame-skipper.ts
@@ -0,0 +1,27 @@
+export class FrameSkipper {
+  private frame: number;
+  private currentSkipFrame: number = 0;
+  constructor(frameToSkip: number) {
+    this.frame = frameToSkip;
+    this.currentSkipFrame = frameToSkip;
+  }
+
+  public get shouldSkipFrame(): boolean {
+    if (this.frame == 0) return false;
+    this.currentSkipFrame--;
+    if (this.currentSkipFrame <= 0) {
+      this.currentSkipFrame = this.frame;
+      return false;
+    }
+    return true;
+  }
+
+  public set frameToSkip(value: number) {
+    this.frame = value;
+    this.currentSkipFrame = 0;
+  }
+
+  public get isSkip() {
+    return this.currentSkipFrame - 1 > 0;
+  }
+}
