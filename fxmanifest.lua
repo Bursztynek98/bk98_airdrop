e96280dcd21b80be7921e242994c485faed4003d
Bursztynek98 <44409970+Bursztynek98@users.noreply.github.com>
2024-01-01 05:07:07 +0100

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+---@diagnostic disable: undefined-global
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'You'
+version '1.0.0'
+
+fxdk_watch_command 'yarn' { 'watch' }
+fxdk_build_command 'yarn' { 'build' }
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
