b3ca0101528cdb320eeb9e8793a7002677a16b5c
Bursztynek98 <44409970+Bursztynek98@users.noreply.github.com>
2024-01-11 20:08:01 +0100

    Add author for fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author 'You'
+author 'Bursztynek98'
 version '1.0.0'
 
 fxdk_watch_command 'yarn' { 'watch' }
@@ -11,11 +11,11 @@ fxdk_build_command 'yarn' { 'build' }
 
 client_scripts {
   'dist/client.js',
-  'example/client/notification.lua',
-  'example/client/target.lua',
-  'example/client/blip.lua'
+  -- 'example/client/notification.lua',
+  -- 'example/client/target.lua',
+  -- 'example/client/blip.lua'
 }
 server_scripts {
   'dist/server.js',
-  'example/server/main.lua'
+  -- 'example/server/main.lua'
 }
